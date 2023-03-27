a9585535e1303166702f4c4322b6574695714c26
JavierMunho <javiermunho@gmail.com>
2023-03-27 07:20:57 -0300

    primer commit,definicion de clases

---
+++ src/main/java/com/mycompany/prode/Prode.java
@@ -0,0 +1,20 @@
+
+
+package com.mycompany.prode;
+
+import java.util.Scanner;
+
+
+public class Prode  {
+
+    public static void main(String[] args) {
+  
+      Scanner entrada = new Scanner (System.in);
+      
+      Partido partido1 = new Partido();
+  Equipo equipo1 = new Equipo("Argentina","seleccion");
+  Equipo equipo2 = new Equipo("Arabia saudita","seleccion"); 
+      Partido partido2 = new Partido();
+
+}
+}
