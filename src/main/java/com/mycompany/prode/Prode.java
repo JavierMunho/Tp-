ff995ea814d88ec68a4911ffde5085eaea6c06db
JavierMunho <javiermunho@gmail.com>
2023-04-26 00:12:09 -0300

    con erores segunda parte se agregaron ronda y participantes

--- src/main/java/com/mycompany/prode/Prode.java
+++ src/main/java/com/mycompany/prode/Prode.java
@@ -6,11 +6,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Prode {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		// Leer resultados
 		Collection<Partido> partidos = new ArrayList<Partido>();
 
@@ -29,20 +31,25 @@ public class Prode {
 			if (primera) {
 				primera = false;
 			} else {
-				// Argentina,1,2,Arabia Saudita
+				// partidos de ronda
 				String[] campos = lineaResultado.split(",");
 				Equipo equipo1 = new Equipo(campos[0]);
 				Equipo equipo2 = new Equipo(campos[3]);
 				Partido partido = new Partido(equipo1, equipo2);
 				partido.setGolesEq1(Integer.parseInt(campos[1]));
 				partido.setGolesEq2(Integer.parseInt(campos[2]));
+				partido.setRonda(Integer.parseInt(campos[4]));
 				partidos.add(partido);
 			}
 
 		}
 		// Leer pronostico
-		int puntos = 0; // total puntos pesona
-		
+
+		Map<String, Integer> puntosParticipante = new HashMap<>();
+
+
+		//int puntos = 0; // total puntos persona
+
 		Path pathPronostico = Paths.get(args[1]);
 		List<String> lineasPronostico = null;
 		try {
@@ -64,41 +71,58 @@ public class Prode {
 
 				for (Partido partidoCol : partidos) {
 					if (partidoCol.getEquipo1().getNombre(
-							).equals(equipo1.getNombre())
+					).equals(equipo1.getNombre())
 							&& partidoCol.getEquipo2().getNombre(
-									).equals(equipo2.getNombre())) {
-						
+					).equals(equipo2.getNombre())) {
+
 						partido = partidoCol;
-						
+
 					}
 				}
 				Equipo equipo = null;
 				EnumResultado resultado = null;
-				if("X".equals(campos[1])) {
+				if ("X".equals(campos[1])) {
 					equipo = equipo1;
 					resultado = EnumResultado.GANADOR;
 				}
-				if("X".equals(campos[2])) {
+				if ("X".equals(campos[2])) {
 					equipo = equipo1;
 					resultado = EnumResultado.EMPATE;
 				}
-				if("X".equals(campos[3])) {
+				if ("X".equals(campos[3])) {
 					equipo = equipo1;
 					resultado = EnumResultado.PERDEDOR;
 				}
 				Pronostico pronostico = new Pronostico(partido, equipo, resultado);
 				// sumar los puntos correspondientes
-				puntos += pronostico.puntos();
+
+
+				String nombreParticipante = campos[5];
+				if (puntosParticipante.containsKey(
+						nombreParticipante)) {
+					puntosParticipante.put(nombreParticipante,
+							puntosParticipante.get(
+									nombreParticipante)
+									+
+									pronostico.puntos()
+					);
+				} else {
+					puntosParticipante.put(nombreParticipante,
+							pronostico.puntos());
+				}
 			}
 		}
 
 		// mostrar los puntos
-		System.out.println("Los puntos obtenidos por el apostador fueron:");
-		System.out.println(puntos);
+		for (String participante : puntosParticipante.keySet()) {
+			System.out.println("Los puntos obtenidos por el Apostador " + participante + " fueron:");
+			System.out.println(puntosParticipante.get(participante));
 
+		}
 	}
+}
 
 
             
    
-}
+
